3cb4001bdbc3c952215514886029fd5b875441b2
k-corby <57394646+k-corby@users.noreply.github.com>
2020-10-26 11:02:05 +1000

    FC

---
+++ api/app/views/home/index.php
@@ -0,0 +1,67 @@
+<div class="row-fixed header">
+    <div>
+        <h1>Welcome to Algo.<br>
+            Exploring <u>algorithms</u> <br>and <u>systems</u> problems.</h1>
+        <p>Learn how to solve simple to complex programming problems.</p>
+        <p>
+            <br>
+            <a class="btn btn-fixed" href="/home/philosophy">Learn now &rarr;</a>
+        </p>
+    </div>
+</div>
+<div class="row-fluid img-fluid">
+    <article>
+        <img src='/img/programming.jpg'>
+    </article>
+    <article>
+        <img src="/img/banner/ui-design.jpg">
+        <div class="overlay overlay-show bg-orange">
+            <div class="text">
+                <h1>Depth First Search</h1>
+                <p class="btn">Learn more</p>
+                <button class=""></button>
+            </div>
+        </div>
+    </article>
+    <article>
+        <img src='/img/banner/social.jpg'>
+    </article>
+    <article>
+        <img src='/img/banner/node.png'>
+        <div class="overlay overlay-show bg-purple">
+            <div class="text">
+                <h1>Depth First Search</h1>
+                <p class="btn bg-black">Learn more</p>
+                <button class=""></button>
+            </div>
+        </div>
+    </article>
+    <article>
+        <img src='/img/banner/chip.jpg'>
+    </article>
+    <article>
+        <img src='/img/banner/data-model.jpg'>
+        <div class="overlay overlay-show bg-yellow">
+            <div class="text">
+                <h1>Depth First Search</h1>
+                <p class="btn bg-black">Learn more</p>
+                <button class=""></button>
+            </div>
+        </div>
+    </article>
+</div>
+<div class="row-fluid" style="margin-top:250px;">
+    <div>
+        <img width="100%" src="/img/services.png">
+    </div>
+    <div class="row-space-right">
+        <h1 class="mrg-top">
+            Featured<br> algorithms
+            <!--Learn a variety<br> of algorithmic & pseudocode concepts.-->
+        </h1>
+        <p>We break down programming concepts to simple pseudocode and then show you how to program them.</p>
+        <p class="mrg-top">
+            <a class="btn btn-fixed" href="/home/philosophy">Learn now &rarr;</a>
+        </p>
+    </div>
+</div>
---
+++ api/public/js/custom.js
@@ -0,0 +1,10 @@
+/** GENERALS */
+
+function toggle() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
