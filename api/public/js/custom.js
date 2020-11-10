8a4ff299c5687e68a89868f438a1662667073a8c
k-corby <57394646+k-corby@users.noreply.github.com>
2020-11-10 20:20:01 +1000

    v4

--- api/public/js/custom.js
+++ api/public/js/custom.js
@@ -8,3 +8,11 @@ function toggle() {
         x.className = "nav";
     }
 }
+
+//$(document).ready(function () {
+//  $(window).scroll(function () {
+//$(".row-fixed.header").css("margin-top", 0 + $(window).scrollTop() * 0.1);
+//$(".row-fixed.header").css("transform", "translate3d(0px, " + -($(window).scrollTop() * 0.2).toFixed() + "px, 0px)");
+        //$(".row-fixed.header").css("opacity", 1 - $(window).scrollTop() / 2 / $('.row-fixed.header').height());
+    //});
+//});
