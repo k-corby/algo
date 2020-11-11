462f95028162c0ae843eb8086a8d1b1023749de0
k-corby <57394646+k-corby@users.noreply.github.com>
2020-11-11 12:17:46 +1000

    vue

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+import './assets/css/style.css'
+import './assets/css/grid.css'
+
+createApp(App).use(store).use(router).mount('#app')
